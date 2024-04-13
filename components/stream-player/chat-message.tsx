fb7094d8790d23dea99507de21760d577db31d00
Allon Nam <allon.nam96@gmail.com>
2024-04-13 19:58:29 -0400

    community component

--- components/stream-player/chat-message.tsx
+++ components/stream-player/chat-message.tsx
@@ -15,16 +15,18 @@ export const ChatMessage = ({
     const color = stringToColor(data.from?.name || "")
     return (
         <div className="flex gap-2 p-2 rounded-md hover:bg-white/5">
+            <div className="flex flex-wrap items-baseline gap-1 grow">
             <p className="text-sm text-white/40">
                 {format(data.timestamp, "HH:MM")}
             </p>
-            <div className="flex flex-wrap items-baseline gap-1 grow">
                 <p className="text-sm font-seminbold whitespace-nowrap">
                     <span className="truncate" style={{ color: color }}>
                         {data.from?.name}
-                    </span>
+                    </span>:
                 </p>
-                <p className="test-sm break-all"> {data.message}</p>
+                <p className="test-sm break-all">
+                    {data.message}
+                </p>
             </div>
         </div>
     );
--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -7,8 +7,8 @@ import { cn } from "@/lib/utils";
 import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { useViewerToken } from "@/hooks/user-viewer-token";
 
-import { Video } from "./video";
-import { Chat } from "./chat";
+import { Video, VideoSkeleton } from "./video";
+import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
 
 interface StreamPlayerProps {
@@ -30,20 +30,16 @@ export const StreamPlayer = ({
     const { collapsed } = useChatSidebar((state) => state)
 
     if (!token || !name || !identity) {
-        return (
-            <div>
-                Cannot watch the stream
-            </div>
-        )
+        return <StreamPlayerSkeleton />
     }
 
     return (
         <>
-        {collapsed && (
-            <div className="hidden lg:block fixed top-[100px] right-2 z-50">
-                <ChatToggle />
-            </div>
-        )}
+            {collapsed && (
+                <div className="hidden lg:block fixed top-[100px] right-2 z-50">
+                    <ChatToggle />
+                </div>
+            )}
             <LiveKitRoom
                 token={token}
                 serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
@@ -78,3 +74,17 @@ export const StreamPlayer = ({
         </>
     );
 };
+
+export const StreamPlayerSkeleton = () => {
+    return (
+        <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+            <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+                <VideoSkeleton />
+                {/* todo header sk */}
+            </div>
+            <div className="col-span-1 bg-background">
+                <ChatSkeleton />
+            </div>
+        </div>
+    )
+}
