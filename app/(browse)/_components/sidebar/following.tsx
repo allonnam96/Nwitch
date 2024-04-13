2a607fe3b5b430facc4ebd62243cd413aedc3be6
Allon Nam <allon.nam96@gmail.com>
2024-04-12 23:39:38 -0400

    livekit webhook

--- app/(browse)/_components/sidebar/following.tsx
+++ app/(browse)/_components/sidebar/following.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { Follow, User } from "@prisma/client";
+import { Follow, Stream, User } from "@prisma/client";
 
 import { useSidebar } from "@/store/use-sidebar";
 
 import { UserItem, UserItemSkeleton } from "./user-item";
 
 interface FollowingProps {
-    data: (Follow & { following: User })[];
+    data: (Follow & {
+        following: User & {
+            stream: Stream | null;
+        },
+    })[];
 }
 
 export const Following = ({
@@ -34,6 +38,7 @@ export const Following = ({
                         key={follow.following.id}
                         username={follow.following.username}
                         imageUrl={follow.following.imageUrl}
+                        isLive={follow.following.stream?.isLive}
                     />
                 ))}
             </ul>
@@ -45,7 +50,7 @@ export const FollowingSkeleton = () => {
     return (
         <ul className="px-2 pt-2 lg:pt-0">
             {[...Array(3)].map((_, i) => (
-                <UserItemSkeleton key={i}/>
+                <UserItemSkeleton key={i} />
             ))}
         </ul>
     )
