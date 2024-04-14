a8ebb10d1c0011101606ee8c0f5b6fc94ef87ed5
Allon Nam <allon.nam96@gmail.com>
2024-04-14 14:26:48 -0400

    unblock page

--- app/(browse)/search/_components/result-card.tsx
+++ app/(browse)/search/_components/result-card.tsx
@@ -1,12 +1,19 @@
 import { Thumbnail, ThumbnailSkeleton } from "@/components/thumbnail";
 import { VerifiedMark } from "@/components/verified-mark";
-import { Stream, User } from "@prisma/client";
+import { User } from "@prisma/client";
 import { formatDistanceToNow } from "date-fns";
 import { Skeleton } from "@/components/ui/skeleton";
 import Link from "next/link";
 
 interface ResultCardProps {
-    data: Stream & { user: User };
+    data: {
+        id: string;
+        name: string;
+        thumbnailUrl: string | null;
+        isLive: boolean;
+        updatedAt: Date;
+        user: User;
+    };
 };
 
 export const ResultCard = ({
@@ -16,7 +23,7 @@ export const ResultCard = ({
         <Link href={`/${data.user.username}`}>
             <div className="w-full flex gap-x-4">
                 <div className="relative h-[9rem] w-[16rem]">
-                    <Thumbnail 
+                    <Thumbnail
                         src={data.thumbnailUrl}
                         fallback={data.user.imageUrl}
                         isLive={data.isLive}
@@ -25,10 +32,10 @@ export const ResultCard = ({
                 </div>
                 <div className="space-y-1">
                     <div className="flex items-center gap-x-2">
-                    <p className="font-bold text-lg cursor-pointer hover:text-blue-500">
-                        {data.user.username}  
-                    </p>
-                    <VerifiedMark />
+                        <p className="font-bold text-lg cursor-pointer hover:text-blue-500">
+                            {data.user.username}
+                        </p>
+                        <VerifiedMark />
                     </div>
                     <p className="text-sm text-muted-foreground">{data.name}</p>
                     <p className="text-sm text-muted-foreground">
